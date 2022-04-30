21aa601fba96da4e737b20942c7b316ea2b5aac7
Cristiana99 <cristianagabriela99@gmail.com>
2022-04-30 18:37:29 +0300

    Initial Commit

---
+++ RentNGoApp/RentNGoApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace RentNGoApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
