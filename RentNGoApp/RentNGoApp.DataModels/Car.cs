f2c0515d75e5c2cce08cbfaee828f1cfcb193349
shirounk01 <raresmihai18@gmail.com>
2022-05-01 18:44:12 +0300

    The design of the feed page

--- RentNGoApp/RentNGoApp.DataModels/Car.cs
+++ RentNGoApp/RentNGoApp.DataModels/Car.cs
@@ -1,17 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace RentNGoApp.DataModels
 {
+    public enum Brand
+    {
+        [Display(Name = "Choose...")] None, Opel, Dacia, Audi, Tesla, BMW, Ford, Toyota, Volkswagen, MercedesBenz
+    }
+    public enum Color
+    {
+        [Display(Name = "Choose...")] None, Red, Blue, Green, White, Black, Yellow, Grey, Orange, Silver, Gold
+    }
     public class Car
     {
         public int carId { get; set; }
         public string name { get; set; }
-        public string brand { get; set; }
-        public string color { get; set; }
+        public Brand brand { get; set; }
+        public Color color { get; set; }
         public double price { get; set; }
         public string description { get; set; }
         public ICollection<Image> images { get; set; }
---
+++ RentNGoApp/RentNGoApp/ViewModels/Feed.cs
@@ -0,0 +1,10 @@
+﻿using RentNGoApp.DataModels;
+
+namespace RentNGoApp.ViewModels
+{
+    public class Feed
+    {
+        public Filter filter { get; set; }
+        public ICollection<Car> cars { get; set; }
+    }
+}
