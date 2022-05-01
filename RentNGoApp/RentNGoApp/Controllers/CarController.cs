23a713095b7235ddd3f472eb4ee9635762245558
Cristiana99 <cristianagabriela99@gmail.com>
2022-05-01 21:22:47 +0300

    The design of the announcement - details page and add test image

--- RentNGoApp/RentNGoApp/Controllers/CarController.cs
+++ RentNGoApp/RentNGoApp/Controllers/CarController.cs
@@ -30,6 +30,11 @@ namespace RentNGoApp.Controllers
             return View();
         }
 
+        public IActionResult Details()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
