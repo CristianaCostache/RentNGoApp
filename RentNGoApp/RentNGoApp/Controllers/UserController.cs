c796991d877b86d356948a717d85dd1255dd7c64
shirounk01 <raresmihai18@gmail.com>
2022-05-01 19:56:50 +0300

    The design of the profile page

---
+++ RentNGoApp/RentNGoApp/Controllers/UserController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RentNGoApp.DataModels;
+using RentNGoApp.Models;
+using System.Diagnostics;
+
+namespace RentNGoApp.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly ILogger<UserController> _logger;
+
+        public UserController(ILogger<UserController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Profile()
+        {
+            return View();
+        }
+        
+    }
+}
